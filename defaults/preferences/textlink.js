5576172cd5ab9a5a87fdfbb480e13f56fbfd086c
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2007-05-11 20:33:53 +0000

    拡張機能の名前もローカライズするようにした
    git-svn-id: https://www.cozmixng.org/repos/piro/textlink/trunk@566 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- defaults/preferences/textlink.js
+++ defaults/preferences/textlink.js
@@ -1,1 +1,2 @@
+pref("extensions.{54BB9F3F-07E5-486c-9B39-C7398B99391C}.name", "chrome://textlink/locale/textlink.properties") ;
 pref("extensions.{54BB9F3F-07E5-486c-9B39-C7398B99391C}.description", "chrome://textlink/locale/textlink.properties") ;
