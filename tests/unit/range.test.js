3afb34a0481c40a7e9eb643942e8a2e552ab14b0
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2009-02-13 11:07:24 +0000

    Range関係のテストの枠組みだけ
    git-svn-id: https://www.cozmixng.org/repos/piro/textlink/trunk@3656 599a83e7-65a4-db11-8015-0010dcdd6dc2

---
+++ tests/unit/range.test.js
@@ -0,0 +1,26 @@
+utils.include('common.inc.js');
+
+function setUp()
+{
+	sv = getNewService();
+}
+
+function tearDown()
+{
+}
+
+function test_getSelectionURIRanges()
+{
+}
+
+function test_getURIRangesFromRange()
+{
+}
+
+function test_getFindRange()
+{
+}
+
+function test_shrinkURIRange()
+{
+}
