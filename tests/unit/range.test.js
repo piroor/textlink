40ce1ead995ee16e2d66e2c31d11f85b9cb12949
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2009-02-13 11:25:17 +0000

    shrinkURIRange()のテスト
    git-svn-id: https://www.cozmixng.org/repos/piro/textlink/trunk@3659 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- tests/unit/range.test.js
+++ tests/unit/range.test.js
@@ -10,6 +10,11 @@ function tearDown()
 {
 }
 
+function $(aId)
+{
+	return content.document.getElementById(aId);
+}
+
 function test_getSelectionURIRanges()
 {
 }
@@ -24,4 +29,11 @@ function test_getFindRange()
 
 function test_shrinkURIRange()
 {
+	var range = content.document.createRange();
+	var node = $('first').firstChild;
+	range.setStart(node, 8);
+	range.setEnd(node, 33);
+	assert.equals('(http://www.mozilla.org/)', range.toString());
+	range = sv.shrinkURIRange(range);
+	assert.equals('http://www.mozilla.org/', range.toString());
 }
