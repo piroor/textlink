336f9faaecc6ea61a7eb135fab077b4929e7a603
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2009-03-17 12:24:34 +0000

    Firefox 3でtableのセルの内容が分割されるかどうかのテスト追加
    git-svn-id: https://www.cozmixng.org/repos/piro/textlink/trunk@3826 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- tests/unit/range.test.js
+++ tests/unit/range.test.js
@@ -1,5 +1,8 @@
 utils.include('common.inc.js');
 
+var textEncoderEnabled = '@mozilla.org/layout/documentEncoder;1?type=text/plain' in Cc &&
+						'nsIDocumentEncoder' in Ci;
+
 function setUp()
 {
 	utils.setPref('intl.accept_languages', 'ja,en-us,en');
@@ -57,8 +60,6 @@ function test_getSelection()
 	);
 }
 
-var textEncoderEnabled = '@mozilla.org/layout/documentEncoder;1?type=text/plain' in Cc &&
-						'nsIDocumentEncoder' in Ci;
 function test_getTextContentFromRange()
 {
 	var range = content.document.createRange();
@@ -370,6 +371,35 @@ function test_getURIRangesFromRange()
 	ranges = sv.getURIRangesFromRange(range, sv.FIND_LAST);
 	assert.equals(['ttp://ftp.netscape.com/pub/netscape6/'], ranges.map(rangesToString));
 	assert.equals(['http://ftp.netscape.com/pub/netscape6/'], ranges.map(rangesToURI));
+
+
+	if (textEncoderEnabled) {
+		range.setStart($('table-cell1').firstChild, 3);
+		range.setEnd($('table-cell6').firstChild, 8);
+		ranges = sv.getURIRangesFromRange(range);
+		assert.equals(
+			[
+				'http://piro.sakura.ne.jp/latest/',
+				'http://piro.sakura.ne.jp/latest/blosxom/mozilla/',
+				'http://piro.sakura.ne.jp/latest/blosxom/mozilla/xul/',
+				'ttp://piro.sakura.ne.jp/latest/blosxom/webtech/',
+				'ttp://piro.sakura.ne.jp/xul/',
+				'ttp://piro.sakura.ne.jp/xul/tips/'
+			],
+			ranges.map(rangesToString)
+		);
+		assert.equals(
+			[
+				'http://piro.sakura.ne.jp/latest/',
+				'http://piro.sakura.ne.jp/latest/blosxom/mozilla/',
+				'http://piro.sakura.ne.jp/latest/blosxom/mozilla/xul/',
+				'http://piro.sakura.ne.jp/latest/blosxom/webtech/',
+				'http://piro.sakura.ne.jp/xul/',
+				'http://piro.sakura.ne.jp/xul/tips/'
+			],
+			ranges.map(rangesToURI)
+		);
+	}
 
 
 	var selection;
