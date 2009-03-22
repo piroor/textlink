95dbed5a09e6fd681284315d7ee33ab8ecbaa13a
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2009-03-22 16:05:29 +0000

    動作するようにした
    git-svn-id: https://www.cozmixng.org/repos/piro/textlink/trunk@3835 599a83e7-65a4-db11-8015-0010dcdd6dc2

--- content/textlink/messengerOverlay.js
+++ content/textlink/messengerOverlay.js
@@ -1,8 +1,8 @@
 var TextLinkMessengerService = { 
-	 
+	
 	get browser() 
 	{
-		return document.getElementById('content');
+		return document.getElementById('messagepane');
 	},
  
 	handleEvent : function(aEvent) 
@@ -10,7 +10,12 @@ var TextLinkMessengerService = {
 		switch (aEvent.type)
 		{
 			case 'load':
-				this.init();
+				if (aEvent.currentTarget == this.browser) {
+					this.onContentLoad();
+				}
+				else {
+					this.init();
+				}
 				return;
 
 			case 'unload':
@@ -18,7 +23,66 @@ var TextLinkMessengerService = {
 				return;
 		}
 	},
- 	
+ 
+	onContentLoad : function() 
+	{
+		this.unlinkifyAutoLinks();
+		this.linkify();
+	},
+	
+	unlinkifyAutoLinks : function() 
+	{
+		var doc = this.browser.contentDocument;
+		var links = this.evaluateXPath(
+				'/descendant::*[not('+(
+					'addbook,imap,mailbox,mailto,news,nntp,pop,snews,feed'.split(',')
+					.map(function(aSchemer) {
+						return 'starts-with(@href, "'+aSchemer+':")';
+					})
+					.join(' or ')
+				)+') and @href=text()]',
+				doc
+			);
+		var range = doc.createRange();
+		for (let i = links.snapshotLength-1; i > -1; i--)
+		{
+			let link = links.snapshotItem(i);
+			range.selectNodeContents(link);
+			let contents = range.extractContents();
+			range.selectNode(link);
+			range.deleteContents();
+			range.insertNode(contents);
+		}
+		range.detach();
+	},
+ 
+	linkify : function() 
+	{
+		var doc = this.browser.contentDocument;
+		var range = doc.createRange();
+		range.selectNodeContents(doc.body);
+		var uriRanges = this.getURIRangesFromRange(range);
+		uriRanges.reverse().forEach(function(aRange) {
+			if (!this.getParentLink(aRange.range.startContainer)) {
+				let text = aRange.range.toString();
+				let link = doc.createElement('a');
+				link.setAttribute('href', aRange.uri);
+				link.appendChild(aRange.range.extractContents());
+				aRange.range.insertNode(link);
+			}
+			aRange.range.detach();
+		}, this);
+		range.detach();
+	},
+	getParentLink : function(aNode)
+	{
+		return this.evaluateXPath(
+				'ancestor-or-self::*[@href]',
+				aNode,
+				XPathResult.FIRST_ORDERED_NODE_TYPE
+			).singleNodeValue;
+	},
+  
 	init : function() 
 	{
 		window.removeEventListener('load', this, false);
@@ -27,7 +91,7 @@ var TextLinkMessengerService = {
 		this.addPrefListener(this);
 		this.initPrefs();
 
-		this.initBrowser(gBrowser);
+		this.browser.addEventListener('load', this, true);
 	},
  
 	destroy : function() 
@@ -36,7 +100,7 @@ var TextLinkMessengerService = {
 
 		this.removePrefListener(this);
 
-		this.destroyBrowser(gBrowser);
+		this.browser.removeEventListener('load', this, true);
 	}
   
 }; 
