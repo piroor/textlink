22740cfdd5995a97b7ab9dc572600bb4930fd5bd
YUKI Hiroshi <shimoda@clear-code.com>
2017-10-30 17:37:15 +0900

    Add script to handle double click action

---
+++ webextensions/common/common.js
@@ -0,0 +1,59 @@
+/*
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+'use strict';
+
+var configs;
+var gLogContext = '?';
+
+function log(aMessage, ...aArgs)
+{
+  if (!configs || !configs.debug)
+    return;
+
+  var nest   = (new Error()).stack.split('\n').length;
+  var indent = '';
+  for (let i = 0; i < nest; i++) {
+    indent += ' ';
+  }
+  console.log(`tst<${gLogContext}>: ${indent}${aMessage}`, ...aArgs);
+}
+
+async function wait(aTask = 0, aTimeout = 0) {
+  if (typeof aTask != 'function') {
+    aTimeout = aTask;
+    aTask    = null;
+  }
+  return new Promise((aResolve, aReject) => {
+    setTimeout(async () => {
+      if (aTask)
+        await aTask();
+      aResolve();
+    }, aTimeout);
+  });
+}
+
+function nextFrame() {
+  return new Promise((aResolve, aReject) => {
+    window.requestAnimationFrame(aResolve);
+  });
+}
+
+function clone(aOriginalObject, aExtraProperties) {
+  var cloned = {};
+  for (let key of Object.keys(aOriginalObject)) {
+    cloned[key] = aOriginalObject[key];
+  }
+  if (aExtraProperties) {
+    for (let key of Object.keys(aExtraProperties)) {
+      cloned[key] = aExtraProperties[key];
+    }
+  }
+  return cloned;
+}
+
+configs = new Configs({
+  debug: true
+});
