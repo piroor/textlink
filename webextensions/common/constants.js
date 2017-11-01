ac9c32f5b88cadcd07a14f1859b9f2e056ec64aa
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2017-11-01 21:33:59 +0900

    Align

--- webextensions/common/constants.js
+++ webextensions/common/constants.js
@@ -5,9 +5,9 @@
 */
 'use strict';
 
-const kCOMMAND_TRY_ACTION      = 'textlink:try-action';
-const kCOMMAND_FIND_URI_RANGES = 'textlink:find-uri-ranges';
-const kCOMMAND_OPEN_URIS       = 'textlink:open-uris';
+const kCOMMAND_TRY_ACTION       = 'textlink:try-action';
+const kCOMMAND_FIND_URI_RANGES  = 'textlink:find-uri-ranges';
+const kCOMMAND_OPEN_URIS        = 'textlink:open-uris';
 const kCOMMAND_FETCH_URI_RANGES = 'textlink:fetch-uri-ranges';
 
 const kACTION_DISABLED               = 0;
