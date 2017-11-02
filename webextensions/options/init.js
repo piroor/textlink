d83e19a8a9e1f0af0535b0cbba488fdb8f0fdb33
YUKI Hiroshi <shimoda@clear-code.com>
2017-11-03 00:57:29 +0900

    Generate options dynamically

--- webextensions/options/init.js
+++ webextensions/options/init.js
@@ -27,9 +27,70 @@ function onConfigChanged(aKey) {
   }
 }
 
+function actionGroup(aParams) {
+  return `
+    <h2>__MSG_config.action.group.${aParams.group}__</h2>
+    ${aParams.content}
+  `;
+}
+
+function actionFieldSet(aParams) {
+  return `
+    <fieldset class="action-definition">
+      <legend>__MSG_config.action.${aParams.action}__</legend>
+      ${aParams.content}
+    </fieldset>
+  `;
+}
+
+function actionCheckboxes(aParams) {
+  var base   = aParams.base;
+  var action = aParams.action;
+  var type   = aParams.type;
+  return `
+    <p><label><input id="${base}_${action}_${type}"
+                     type="checkbox">
+              __MSG_config.trigger.${type}__</label>
+       <span class="delimiter">-</span>
+       <label><input id="${base}_${action}_${type}_alt"
+                     type="checkbox">
+              __MSG_config.trigger.alt__</label>
+       <span class="delimiter">-</span>
+       <label><input id="${base}_${action}_${type}_ctrl"
+                     type="checkbox">
+              __MSG_config.trigger.ctrl__</label>
+       <span class="delimiter">-</span>
+       <label><input id="${base}_${action}_${type}_meta"
+                     type="checkbox">
+              __MSG_config.trigger.meta__</label>
+       <span class="delimiter">-</span>
+       <label><input id="${base}_${action}_${type}_shift"
+                     type="checkbox">
+              __MSG_config.trigger.shift__</label></p>
+  `;
+}
+
 configs.$addObserver(onConfigChanged);
 window.addEventListener('DOMContentLoaded', () => {
   configs.$loaded.then(() => {
+    var fragment = document.createDocumentFragment();
+    var range = document.createRange();
+    range.selectNodeContents(document.querySelector('#actions'));
+    range.insertNode(range.createContextualFragment(
+      ['action', 'actionInEditable'].map(base =>
+        actionGroup({
+          group: base,
+          content: ['select', 'current', 'tab', 'tabBackground', 'window', 'copy'].map(action =>
+            actionFieldSet({
+              action,
+              content: ['dblclick', 'enter'].map(type =>
+                actionCheckboxes({ base, action, type })).join('\n')
+            })).join('\n')
+        })).join('\n')
+    ));
+    range.detach();
+    l10n.updateDocument();
+
     options.buildUIForAllConfigs(document.querySelector('#debug-configs'));
     onConfigChanged('debug');
 
